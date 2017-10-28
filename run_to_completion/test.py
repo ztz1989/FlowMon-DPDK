d31f8ce7d736a863c861b32aee8048fc97cc779f
ztz1989 <zhuzhuaiqiqi1314@gmail.com>
2017-10-28 21:58:23 +0200

    Second commit

---
+++ run_to_completion/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# The script to test the RX functionality
+
+import subprocess, time, signal, random
+
+for _ in range(50):
+	p = subprocess.Popen(["./build/FlowCount", "-c", "111"])
+	time.sleep(370)
+	p.send_signal(signal.SIGINT)
+	time.sleep(6)
