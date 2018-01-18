65c27641f58c2ab7eb8c3f44d2557c34db1f1866
ztz1989 <zhuzhuaiqiqi1314@gmail.com>
2018-01-18 10:27:35 +0100

    Rename APP

--- run_to_completion/test.py
+++ run_to_completion/test.py
@@ -4,7 +4,7 @@
 import subprocess, time, signal, random
 
 for _ in range(50):
-	p = subprocess.Popen(["./build/FlowCount", "-c", "111"])
+	p = subprocess.Popen(["./build/FlowMon-DPDK", "-c", "111"])
 	time.sleep(370)
 	p.send_signal(signal.SIGINT)
 	time.sleep(6)
